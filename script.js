c87098e2f719c17a23f7c0b328e8feff10026db5
stephaniemakanaka-coder <stephaniemubayiwa@gmail.com>
2026-05-14 19:31:50 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,41 @@
 const player = document.getElementById("player");
 
-let x = 300;
-let y = 300;
+let x = 400;
+let y = 400;
 
-document.addEventListener("keydown", (e) => {
+document.addEventListener("keydown", (e)=>{
 
-  if(e.key === "w") y -= 10;
-  if(e.key === "s") y += 10;
-  if(e.key === "a") x -= 10;
-  if(e.key === "d") x += 10;
+  if(e.key === "w"){
+    y -= 10;
+  }
+
+  if(e.key === "s"){
+    y += 10;
+  }
+
+  if(e.key === "a"){
+    x -= 10;
+  }
+
+  if(e.key === "d"){
+    x += 10;
+  }
 
   player.style.left = x + "px";
   player.style.top = y + "px";
 
 });
 
-function makeChoice(choice){
+function obeyOrder(){
 
-  if(choice === "obey"){
-    alert("You obeyed the orders.");
-  }
+  alert("You followed the orders.");
+}
 
-  if(choice === "refuse"){
-    alert("You refused the orders. Stress increased.");
-    document.querySelector(".stress").style.width = "80%";
-  }
+function refuseOrder(){
 
+  alert("Stress increased.");
+
+  document.querySelector(".stress-fill").style.width = "85%";
+
+  document.body.style.filter = "grayscale(20%)";
 }
